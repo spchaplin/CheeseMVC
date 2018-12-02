33b57103e9d30768ae337f317c032a3da2f16108
Scott Chaplin <scott.chaplin@gmail.com>
2018-12-01 23:00:01 -0600

    exercise for lesson 8 completed with Edit Cheese ability.

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -1,39 +1,80 @@
-﻿using System;
+﻿using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CheeseMVC.Controllers
 {
-    public class CheeseController : Controller
-    {
+	public class CheeseController : Controller
+	{
 
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			ViewBag.cheeses = CheeseData.GetAll();
 
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            ViewBag.cheeses = Cheeses;
+			return View();
+		}
 
-            return View();
-        }
+		public IActionResult Add()
+		{
+			return View();
+		}
 
-        public IActionResult Add()
-        {
-            return View();
-        }
+		[HttpPost]
+		[Route("/Cheese/Add")]
+		public IActionResult NewCheese(Cheese newCheese)
+		{
 
-        [HttpPost]
-        [Route("/Cheese/Add")]
-        public IActionResult NewCheese(string name, string description = "")
-        {
-            // Add the new cheese to my existing cheeses
-            Cheeses.Add(name, description);
+			/*what framework does (model binding):
+			 * you must have a default constructor for the model object you're trying to bind to the request
+			 Cheese newCheese= new Cheese();
+			 newCheese.Name = Request.get("name");
+			 newCheese.Description = Request.get("description");
+			 */
 
-            return Redirect("/Cheese");
-        }
-    }
+
+			// Add the new cheese to my existing cheeses
+			CheeseData.Add(newCheese);
+
+			return Redirect("/Cheese");
+		}
+
+		public IActionResult Remove()
+		{
+			ViewBag.title = "Remove Cheeses";
+			ViewBag.cheeses = CheeseData.GetAll();
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Remove(int[] cheeseIds)
+		{
+			//TODO- remove cheeses from list
+			foreach (int cheeseId in cheeseIds)
+			{
+				CheeseData.Remove(cheeseId);
+			}
+			return Redirect("/");
+		}
+
+		public IActionResult Edit(int cheeseId)
+		{
+			ViewBag.cheeseToEdit = CheeseData.GetById(cheeseId);
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Edit(int cheeseId, string name, string description)
+		{
+			Cheese cheeseToUpdate = CheeseData.GetById(cheeseId);
+			cheeseToUpdate.Name = name;
+			cheeseToUpdate.Description = description;
+			return Redirect("/");
+		}
+	}
 }
