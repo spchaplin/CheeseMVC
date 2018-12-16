ebca03ed85491e2cc81f199b22fc75e2efac06cf
scott.chaplin <scott.chaplin@gmail.com>
2018-12-16 02:18:02 -0600

    still working on final piece of studio 2

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CheeseMVC.Data;
 using CheeseMVC.Models;
-using System.Collections.Generic;
 using CheeseMVC.ViewModels;
-using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CheeseMVC.Controllers
 {
-    public class CheeseController : Controller
-    {
+	public class CheeseController : Controller
+	{
 		private CheeseDbContext context;
 
 		public CheeseController(CheeseDbContext dbContext)
@@ -16,63 +17,83 @@ namespace CheeseMVC.Controllers
 			context = dbContext;
 		}
 
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            List<Cheese> cheeses = context.Cheeses.ToList();
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			// List<Cheese> cheeses = context.Cheeses.ToList();
+			IList<Cheese> cheeses = context.Cheeses.Include(context => context.Category).ToList();
 
-            return View(cheeses);
-        }
+			return View(cheeses);
+		}
 
-        public IActionResult Add()
-        {
-            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
-            return View(addCheeseViewModel);
-        }
+		public IActionResult Add()
+		{
+			AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel(context.Categories.ToList());
+			return View(addCheeseViewModel);
+		}
 
-        [HttpPost]
-        public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                // Add the new cheese to my existing cheeses
-                Cheese newCheese = new Cheese
-                {
-                    Name = addCheeseViewModel.Name,
-                    Description = addCheeseViewModel.Description,
-                    Type = addCheeseViewModel.Type
-                };
+		[HttpPost]
+		public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
+		{
+			if (ModelState.IsValid)
+			{
 
-                context.Cheeses.Add(newCheese);
+				CheeseCategory newCheeseCategory =
+					context.Categories.Single(c => c.ID == addCheeseViewModel.CategoryID);
+
+				// Add the new cheese to my existing cheeses
+				Cheese newCheese = new Cheese
+				{
+					Name = addCheeseViewModel.Name,
+					Description = addCheeseViewModel.Description,
+					Category = newCheeseCategory
+					//Type = addCheeseViewModel.Type
+				};
+
+				context.Cheeses.Add(newCheese);
 				context.SaveChanges();
 
-                return Redirect("/Cheese");
-            }
+				return Redirect("/Cheese");
+			}
 
-            return View(addCheeseViewModel);
-        }
+			return View(addCheeseViewModel);
+		}
 
-        public IActionResult Remove()
-        {
-            ViewBag.title = "Remove Cheeses";
-            ViewBag.cheeses = context.Cheeses.ToList();
-            return View();
-        }
+		public IActionResult Remove()
+		{
+			ViewBag.title = "Remove Cheeses";
+			ViewBag.cheeses = context.Cheeses.ToList();
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult Remove(int[] cheeseIds)
-        {
-            foreach (int cheeseId in cheeseIds)
-            {
+		[HttpPost]
+		public IActionResult Remove(int[] cheeseIds)
+		{
+			foreach (int cheeseId in cheeseIds)
+			{
 
 				Cheese theCheese = context.Cheeses.Single(c => c.ID == cheeseId);
 				context.Cheeses.Remove(theCheese);
 				//CheeseData.Remove(cheeseId);
-            }
+			}
 
 			context.SaveChanges();
 
-            return Redirect("/");
-        }
-    }
+			return Redirect("/");
+		}
+
+		public IActionResult Category(int id)
+		{
+			if (id == 0)
+			{
+				return Redirect("/Category");
+			}
+
+			CheeseCategory theCategory = context.Categories.Include(cat => cat.Cheeses).Single(cat => cat.ID == id);
+
+			ViewBag.title = "Cheeses in category: " + theCategory.Name;
+
+			return View("Index", theCategory.Cheeses);
+		}
+	}
 }
--- src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CheeseMVC.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,29 +9,40 @@ using System.Threading.Tasks;
 
 namespace CheeseMVC.ViewModels
 {
-    public class AddCheeseViewModel
-    {
-        [Required]
-        [Display(Name = "Cheese Name")]
-        public string Name { get; set; }
+	public class AddCheeseViewModel
+	{
+		[Required]
+		[Display(Name = "Cheese Name")]
+		public string Name { get; set; }
 
-        [Required(ErrorMessage = "You must give your cheese a description")]
-        public string Description { get; set; }
+		[Required(ErrorMessage = "You must give your cheese a description")]
+		public string Description { get; set; }
 
-        public CheeseType Type { get; set; }
+		//public CheeseType Type { get; set; }
 
-        public List<SelectListItem> CheeseTypes { get; set; }
+		//public List<SelectListItem> CheeseTypes { get; set; }
+		[Required]
+		[Display(Name = "Category")]
+		public int CategoryID { get; set; }
 
-        public AddCheeseViewModel() {
+		public List<SelectListItem> Categories { get; set; }
 
-            CheeseTypes = new List<SelectListItem>();
+		public AddCheeseViewModel(IEnumerable<CheeseCategory> categories)
+		{
+			
+			Categories = new List<SelectListItem>();
 
-            // <option value="0">Hard</option>
-            CheeseTypes.Add(new SelectListItem {
-                Value = ((int) CheeseType.Hard).ToString(),
-                Text = CheeseType.Hard.ToString()
-            });
+			//private List<CheeseCategory> list = categories.ToList();
+			//Categories = ((List<SelectListItem>)categories);
+			//Categories = categories;
 
+			// <option value="0">Hard</option>
+			Categories.Add(new SelectListItem
+			{
+				Value = CategoryID.ToString(),
+				Text = Name.ToString()
+			});
+			/*
             CheeseTypes.Add(new SelectListItem
             {
                 Value = ((int)CheeseType.Soft).ToString(),
@@ -41,8 +53,13 @@ namespace CheeseMVC.ViewModels
             {
                 Value = ((int)CheeseType.Fake).ToString(),
                 Text = CheeseType.Fake.ToString()
-            });
+            });*/
 
-        }
-    }
+		}
+		//default constructor necessary for model binding
+		public AddCheeseViewModel()
+		{
+
+		}
+	}
 }
