207fdc39fabd07129d74a4940f77a22169fd5134
Scott Chaplin <scott.chaplin@gmail.com>
2018-12-15 19:44:41 -0600

    Finished studio part 1 - Persisting a Single Class

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -3,13 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC.Data
 {
-    public class CheeseDbContext : DbContext
-    {
-        public DbSet<Cheese> Cheeses { get; set; }
+	public class CheeseDbContext : DbContext
+	{
+		public DbSet<Cheese> Cheeses { get; set; }
 
-        public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
-            : base(options)
-        { }
+		public DbSet<CheeseCategory> Categories { get; set; }
 
-    }
+		public CheeseDbContext(DbContextOptions<CheeseDbContext> options)
+			: base(options)
+		{ }
+
+	}
 }
--- src/CheeseMVC/Migrations/20181216011812_AddCategory.Designer.cs
+++ src/CheeseMVC/Migrations/20181216011812_AddCategory.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181216011812_AddCategory")]
+    partial class AddCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
